72b1d84ebe6d1d4ce0cbdc4a9f739169a4686460
Jervz <jervyariola@gmail.com>
2025-07-05 08:35:19 +0800

    feat(db): install and implement kysely for sql builder

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
-import mysql from 'mysql2';
+import { Kysely, MysqlDialect } from 'kysely';
+import { createPool } from 'mysql2';
 
-const connection = mysql.createConnection({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'root',
-	password: process.env.DB_PASS || 'root',
-	database: process.env.DB_NAME || 'customer_points_system',
+const db = new Kysely({
+	dialect: new MysqlDialect({
+		pool: createPool({
+			host: process.env.DB_HOST || 'localhost',
+			user: process.env.DB_USER || 'root',
+			password: process.env.DB_PASS || 'root',
+			database: process.env.DB_NAME || 'customer_points_system',
+		}),
+	}),
 });
 
-connection.connect(err => {
-	if (err) {
-		console.error('Database connection failed:', err.message);
-		process.exit(1); // Exit process with failure
-	}
-	console.log('✅ Connected to MySQL database.');
-});
-
-export default connection;
+export default db;
