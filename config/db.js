11500adb617b2002f4605958d1fb7c6735fd8f94
Jervz <jervyariola@gmail.com>
2025-07-06 14:53:05 +0800

    chore(db): remove default value for db pool

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 import { Kysely, MysqlDialect } from 'kysely';
 import { createPool } from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = new Kysely({
 	dialect: new MysqlDialect({
 		pool: createPool({
-			host: process.env.DB_HOST || 'localhost',
-			user: process.env.DB_USER || 'root',
-			password: process.env.DB_PASS || 'root',
-			database: process.env.DB_NAME || 'customer_points_system',
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			database: process.env.DB_NAME,
 		}),
 	}),
 });
